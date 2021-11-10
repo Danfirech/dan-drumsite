111833ed69691068406ae2274b0ca86bebb5ca78
Dan Possehl <danielbpossehl@gmail.com>
2021-11-10 16:29:11 -0700

    final commit for day

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 140px;
+  background-color: red;
+  display: flex;
+`;
+
+const Contact = () => {
+  return <Container></Container>;
+};
+
+export default Contact;
