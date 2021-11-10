55049bc77792308978ed888f9d7a92a765d59495
Dan Possehl <danielbpossehl@gmail.com>
2021-11-10 15:40:30 -0700

    Navbar set up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import styled from "styled-components";
+import Navbar from "./components/Navbar";
+import DanFull from "./images/danfull.png";
 
 const Container = styled.div`
   height: 100vh;
   overflow: hidden;
   position: relative;
-  background-color: black;
+  background-color: lightgray;
+  background-image: url(${DanFull});
+  height: 2300px; ;
 `;
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Container></Container>
     </div>
   );
